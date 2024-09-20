ffbc8bd09f254d30c6e5f3297f75a964797e907b
SeKo <kochnev_serezhenka14@mail.ru>
2024-09-20 23:01:50 +0500

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ConfigProvider } from 'antd';
+import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <ConfigProvider>
+      <BrowserRouter>
+        
+      </BrowserRouter>
+    </ConfigProvider>
+  )
+}
+
+export default App
