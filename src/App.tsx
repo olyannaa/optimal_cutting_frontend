4dcdcea45cb168f96b4fbb5a56bc56fbdb41f866
SeKo <kochnev_serezhenka14@mail.ru>
2024-09-23 18:28:36 +0500

    layout page login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import { ConfigProvider } from 'antd';
-import './App.css'
-import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider, ThemeConfig } from 'antd';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Authorization } from './pages/Auth/Authorization';
 
 function App() {
+	const theme: ThemeConfig = {
+		components: {
+			Input: {
+				borderRadius: 2,
+				activeShadow: 'none',
+				fontSize: 16,
+        colorText:'rgba(0, 0, 0, 0.25)'
+			}
+		},
 
-  return (
-    <ConfigProvider>
-      <BrowserRouter>
-        
-      </BrowserRouter>
-    </ConfigProvider>
-  )
+		token: {},
+		cssVar: true,
+	};
+	return (
+		<ConfigProvider theme={theme}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Authorization />} />
+				</Routes>
+			</BrowserRouter>
+		</ConfigProvider>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,21 @@
+import { Button, Form } from 'antd';
+import { LoginInput } from '../custom-input/LoginInput/LoginInput';
+import styles from './LoginForm.module.css'
+
+export const LoginForm=()=>{
+	return(
+		<Form className={styles.loginForm} >
+			<LoginInput
+			name='login'
+			placeholder='логин'
+			/>
+			<LoginInput
+			name='password'
+			placeholder='пароль'
+			/>
+			<Button className={styles.btnLogin} htmlType='submit' type='primary'>
+				Вход
+			</Button>
+		</Form>
+	)
+}
