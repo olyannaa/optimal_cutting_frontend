aaa0577f47c5ab32e850eff22d7c8493988139d9
Arina Topor <arinatopor@abanking.ru>
2024-12-08 18:23:44 +0500

    forms

--- src/components/SelectedMode/SelectedMode.tsx
+++ src/components/SelectedMode/SelectedMode.tsx
@@ -3,7 +3,6 @@ import styles from './SelectedMode.module.css';
 import { useLocation } from 'react-router-dom';
 import { CuttingInfo1D } from '../CuttingInfo1D/CuttingInfo1D';
 import { Cutting2DForm } from '../forms/Cutting2DForm/Cutting2DForm';
-import { FormContainer } from '../FormContainer/FormContainer';
 
 export const SelectedMode = ({
     children,
@@ -15,13 +14,7 @@ export const SelectedMode = ({
             {path === '/cutting/1D' && (
                 <CuttingInfo1D>{children}</CuttingInfo1D>
             )}
-            {path === '/cutting/2D' && (
-                <FormContainer>
-                    <Flex style={{ height: '100%' }}>
-                        <Cutting2DForm></Cutting2DForm>
-                    </Flex>
-                </FormContainer>
-            )}
+            {path === '/cutting/2D' && <Cutting2DForm></Cutting2DForm>}
             {path === '/newDetail' && children}
         </Flex>
     );
